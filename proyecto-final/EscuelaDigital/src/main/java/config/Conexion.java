b927bce954ad6cf039559cd6ff6c25ff0d9d5271
RamiPizasc <ramiropiza97@gmail.com>
2021-11-23 11:58:41 -0300

    primer dia crud

---
+++ proyecto-final/EscuelaDigital/src/main/java/config/Conexion.java
@@ -0,0 +1,46 @@
+package config;
+import java.sql.*;
+
+public class Conexion {
+    public String driver = "com.mysql.cj.jdbc.Driver";
+    public Connection getConexion(){
+        
+        Connection conexion = null;
+        //creamos objeto conexion
+        try{
+            Class.forName(driver);
+            conexion = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/comision1109", 
+                    "ramiropiza", "4931");
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println(e.toString());
+        }
+        return conexion;        
+    }
+    public static void main (String args[]) throws SQLException{
+        Connection c = null;
+        Conexion con = new Conexion();
+        c = con.getConexion();
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        ps = c.prepareStatement("SELECT * FROM participantes");
+        rs = ps.executeQuery();
+        
+        while(rs.next()){
+            //mientras rs tenga más opciones
+            int id = rs.getInt("id");
+            String nombres = rs.getString("nombres");
+            String apellidos = rs.getString("apellidos");
+            String email = rs.getString("email");
+            int telefono = rs.getInt("telefono");
+            
+            System.out.println(
+                      "\nId: " + id 
+                    + "\nNombre:  " + nombres 
+                    + "\nApelllidos: " + apellidos 
+                    + "\nEmail: " + email 
+                    + "\nTelefono: " + telefono);
+        }
+    }
+}
